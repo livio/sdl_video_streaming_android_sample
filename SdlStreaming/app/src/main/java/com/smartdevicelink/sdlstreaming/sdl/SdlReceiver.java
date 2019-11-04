231c414eb224e71275c327c7506595b9a366a5b8
Joey Grover <joeygrover@gmail.com>
2019-11-04 10:20:25 -0500

    Update SdlReceiver with proper foreground start

--- SdlStreaming/app/src/main/java/com/smartdevicelink/sdlstreaming/sdl/SdlReceiver.java
+++ SdlStreaming/app/src/main/java/com/smartdevicelink/sdlstreaming/sdl/SdlReceiver.java
@@ -1,12 +1,18 @@
 package com.smartdevicelink.sdlstreaming.sdl;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 
 import com.smartdevicelink.transport.SdlBroadcastReceiver;
 import com.smartdevicelink.transport.SdlRouterService;
 
 public class SdlReceiver extends SdlBroadcastReceiver {
+
+    private static final String TAG ="SdlReceiver";
+
     public SdlReceiver() {
     }
 
@@ -18,7 +24,22 @@ public class SdlReceiver extends SdlBroadcastReceiver {
 
     @Override
     public void onSdlEnabled(Context context, Intent intent) {
-        intent.setClass(context,SdlService.class);
-        context.startService(intent);
+        Log.d(TAG, "SDL Enabled");
+        intent.setClass(context, SdlService.class);
+
+        // SdlService needs to be foregrounded in Android O and above
+        // This will prevent apps in the background from crashing when they try to start SdlService
+        // Because Android O doesn't allow background apps to start background services
+        ComponentName serviceStarted ;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            serviceStarted = context.startForegroundService(intent);
+        } else {
+            serviceStarted = context.startService(intent);
+        }
+        if(serviceStarted != null){
+            Log.i(TAG, "onSdlEnabled: Service started = " + serviceStarted.flattenToShortString());
+        }else{
+            Log.e(TAG, "onSdlEnabled: Unable to start service");
+        }
     }
 }
