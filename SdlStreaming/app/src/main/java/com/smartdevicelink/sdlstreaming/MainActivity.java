5d9448bf2875855eff2f29fd7c83a92ca69e42d8
“JKAST” <julian.kast@live.com>
2022-10-14 14:11:05 -0400

    Update for SDL 5.6, Target API 33 and request post notification permission

--- SdlStreaming/app/src/main/java/com/smartdevicelink/sdlstreaming/MainActivity.java
+++ SdlStreaming/app/src/main/java/com/smartdevicelink/sdlstreaming/MainActivity.java
@@ -2,6 +2,7 @@ package com.smartdevicelink.sdlstreaming;
 
 import static android.Manifest.permission.BLUETOOTH_CONNECT;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -15,6 +16,8 @@ import androidx.core.content.ContextCompat;
 import com.smartdevicelink.sdlstreaming.sdl.SdlReceiver;
 import com.smartdevicelink.transport.SdlBroadcastReceiver;
 
+import java.util.ArrayList;
+
 /**
  * Using a video view works super easy.
  *
@@ -23,50 +26,83 @@ import com.smartdevicelink.transport.SdlBroadcastReceiver;
 
 public class MainActivity extends Activity {
 
-    private static final int BLUETOOTH_REQUEST_CODE = 200;
+    private static final int REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            if (!checkPermission()) {
-                requestPermission();
-                return;
+        String[] permissionsNeeded = permissionsNeeded();
+        if (permissionsNeeded.length > 0) {
+            requestPermission(permissionsNeeded, REQUEST_CODE);
+            for (String permission : permissionsNeeded) {
+                if (Manifest.permission.BLUETOOTH_CONNECT.equals(permission)) {
+                    // We need to request BLUETOOTH_CONNECT permission to connect to SDL via Bluetooth
+                    return;
+                }
             }
         }
 
         SdlBroadcastReceiver.queryForConnectedService(this);
     }
 
-    private boolean checkPermission() {
-        int btConnectPermission = ContextCompat
-            .checkSelfPermission(getApplicationContext(), BLUETOOTH_CONNECT);
-
-        return btConnectPermission == PackageManager.PERMISSION_GRANTED;
+    /**
+     * Boolean method that checks API level and check to see if we need to request BLUETOOTH_CONNECT permission
+     * @return false if we need to request BLUETOOTH_CONNECT permission
+     */
+    private boolean hasBTPermission() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? checkPermission(Manifest.permission.BLUETOOTH_CONNECT) : true;
     }
 
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{BLUETOOTH_CONNECT},
-                BLUETOOTH_REQUEST_CODE);
+    /**
+     * Boolean method that checks API level and check to see if we need to request POST_NOTIFICATIONS permission
+     * @return false if we need to request POST_NOTIFICATIONS permission
+     */
+    private boolean hasPNPermission() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU ? checkPermission(Manifest.permission.POST_NOTIFICATIONS) : true;
+    }
+
+    private boolean checkPermission(String permission) {
+        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(getApplicationContext(), permission);
+    }
+
+    private void requestPermission(String[] permissions, int REQUEST_CODE) {
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE);
+    }
+
+    private @NonNull String[] permissionsNeeded() {
+        ArrayList<String> result = new ArrayList<>();
+        if (!hasBTPermission()) {
+            result.add(Manifest.permission.BLUETOOTH_CONNECT);
+        }
+        if (!hasPNPermission()) {
+            result.add(Manifest.permission.POST_NOTIFICATIONS);
+        }
+        return (result.toArray(new String[result.size()]));
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
-            case BLUETOOTH_REQUEST_CODE:
+            case REQUEST_CODE:
                 if (grantResults.length > 0) {
-
-                    boolean connectAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    
-                    if (connectAccepted) {
-                        SdlReceiver.queryForConnectedService(this);
-                    }
-
-                    if (!connectAccepted) {
-                        Toast.makeText(this, "BLUETOOTH_CONNECT Permission is needed for Bluetooth testing", Toast.LENGTH_LONG).show();
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (permissions[i].equals(Manifest.permission.BLUETOOTH_CONNECT)) {
+                            boolean btConnectGranted =
+                                    grantResults[i] == PackageManager.PERMISSION_GRANTED;
+                            if (btConnectGranted) {
+                                SdlReceiver.queryForConnectedService(this);
+                            }
+                        } else if (permissions[i].equals(Manifest.permission.POST_NOTIFICATIONS)) {
+                            boolean postNotificationGranted =
+                                    grantResults[i] == PackageManager.PERMISSION_GRANTED;
+                            if (!postNotificationGranted) {
+                                // User denied permission, Notifications for SDL will not appear
+                                // on Android 13 devices.
+                            }
+                        }
                     }
                 }
                 break;
